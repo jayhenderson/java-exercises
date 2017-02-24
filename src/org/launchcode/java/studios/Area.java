2313bd7a4bdd7e63d20c9dee2461cb04fcae20c1
Jay Henderson <jjhenderson@gmail.com>
2017-02-24 14:49:24 -0600

    main function will repeat until valid radius is submitted

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args){
-        System.out.print("Enter a radius: ");
-        Scanner scan = new Scanner(System.in);
-        float userRadius = scan.nextFloat();
+        float userRadius = 0;
+        while (userRadius <= 0)
+        {
+            System.out.print("Enter a radius: ");
+            Scanner scan = new Scanner(System.in);
+            userRadius = scan.nextFloat();
+        }
         double area = 3.14 * userRadius * userRadius;
         System.out.println(String.format("The area of a circle of radius %.2f is: %.2f", userRadius, area));
 
