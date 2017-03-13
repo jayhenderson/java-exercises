482b0f22c9485ab547a9fc847ec04207fcf2bd2e
jayhenderson <jjhenderson@gmail.com>
2017-03-13 17:53:11 -0500

    Pre-class exercises

---
+++ src/org/launchcode/java/demos/java4python/school/Course.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.demos.java4python.school;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by J on 3/13/2017.
+ */
+public class Course {
+
+    private String className;
+    private int classId;
+    private String instructor;
+    private ArrayList<HashMap<String, Integer>> studentList;
+}
